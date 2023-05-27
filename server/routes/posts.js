a5b9ceed30ef47465112d51c28c1aa2aadeb4cca
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-28 00:42:15 +0530

    Added New Data in Backend Part

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This Works!");
-});
+router.get("/", getPosts);
+router.post("/", createPost);
+
 
 export default router;
