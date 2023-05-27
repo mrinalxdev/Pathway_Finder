5e9f39fed1f5e557921e6f8c00d5f1e952bdc4ef
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-28 01:22:05 +0530

    Added New Data Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import memories from './images/memories.png'
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
 
 const App = () => {
   return (
-    <div>
-        <h1>App</h1>
-    </div>
-  )
-}
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Stories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="strectch" spacing={3}>
+            <Grid item xs={12} sm={7}> 
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
-export default App
+export default App;
