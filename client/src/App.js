bcec31439418652628118b0b36595dd3655b201b
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-30 13:12:25 +0530

    Added New classes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from './images/memories.png'
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Stories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Stories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
