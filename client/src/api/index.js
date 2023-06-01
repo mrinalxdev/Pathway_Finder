5b9832afd2615765deb69fb770efc18068787b5a
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-01 06:09:41 +0530

    Added New Data

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'http://localhost:5000/posts'
+const url = "http://localhost:5000/posts";
 
-export const fetchPosts = () => axios.get(url)
+export const fetchPosts = () => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost);
