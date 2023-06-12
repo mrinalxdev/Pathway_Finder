2b71aac26bfb87c8dd00ee36b4b41b3b6ddf4cd2
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-12 17:50:07 +0530

    Added New Data

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image'
-import Paragraph from './components/ui/Paragraph'
-
-export default function Home() {
-  return (
-    <h1 className="bg-red"><Paragraph/></h1>
-  )
-}
